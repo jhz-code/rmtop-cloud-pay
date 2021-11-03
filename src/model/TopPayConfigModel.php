82f155af083cda3347ef31cbdfd1aef4acd976a2
zjh <zhuok520@qq.com>
2021-11-03 16:00:11 +0800

    数据库自动识别

--- src/model/TopPayConfigModel.php
+++ src/model/TopPayConfigModel.php
@@ -7,6 +7,14 @@ use think\Model;
 class TopPayConfigModel extends Model
 {
     // 设置当前模型对应的完整数据表名称
-    protected $table = 'rm_cloud_pay_config';
+    protected $table;
     // 开启自动写入时间戳字段
+    protected $autoWriteTimestamp = true;
+
+    public function __construct(array $data = [])
+    {
+        $this->table = env('database.prefix', '')."cloud_pay_config";
+        parent::__construct($data);
+    }
+
 }
